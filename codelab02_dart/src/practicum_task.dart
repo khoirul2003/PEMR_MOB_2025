074fbf5b12314e6f80d30eb0162dc12ae14ce36b
muathoillah <140579954+muathoillah@users.noreply.github.com>
2025-09-11 22:40:26 +0700

    codelab02_dart

--- codelab02_dart/src/practicum_task.dart
+++ codelab02_dart/src/practicum_task.dart
@@ -2,12 +2,23 @@
 // Modify the code on line 3 in VS Code or your favorite code editor 
 // below to get the desired output!
 
+// void main() {
+//   int umur = 18;
+
+//   for (int i = 0; i < 10; i++){
+//     print('Nama saya adalah Khoirul, sekarang umur ${umur - i}');
+//   }
+
+//   umur = umur;
+// }
+
+// void main() {
+//   String? name; 
+//   print(name); 
+// }
+
 void main() {
-  int umur = 18;
-
-  for (int i = 0; i < 10; i++){
-    print('Nama saya adalah Khoirul, sekarang umur ${umur - i}');
-  }
-
-  umur = umur;
+  late String name;
+  name = "Khoirul";
+  print(name);
 }
